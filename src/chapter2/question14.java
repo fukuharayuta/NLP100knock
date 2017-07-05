1c684fe3193fbd98926bcd1413ef3691dbf56c00
fukuharayuta <fukuhara-yuta-pz@ynu.jp>
2017-07-05 17:28:36 +0900

    question15終了

--- src/chapter2/question14.java
+++ src/chapter2/question14.java
@@ -1,5 +1,7 @@
 package chapter2;
 
+import java.io.*;
+
 /**
  * Created by yuta on 2017/07/05.
  */
@@ -10,11 +12,38 @@ package chapter2;
 public class question14 {
     public static void main(String[] args) {
         String filepath = "res/hightemp.txt";
+        //String outfilepath = "res/question14.txt";
+        int argument = Integer.valueOf(args[0]);
+        headprint(filepath,argument);
+
 
     }
 
-    public static void headprint(String filepath){
-        
+    public static void headprint(String filepath,int argument){
+
+        try{
+            FileReader fr = new FileReader(filepath);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            String newline;
+            line = br.readLine();
+
+            while(line != null){
+                newline = line.substring(0,argument);
+                System.out.println(newline);
+                //pw.println(newline);
+                line = br.readLine();
+            }
+
+            br.close();
+
+        }catch (FileNotFoundException e){
+            System.out.println(e);
+
+        }catch (IOException e){
+            System.out.println(e);
+        }
+
 
     }
 
--- src/chapter2/question15.java
+++ src/chapter2/question15.java
@@ -1,5 +1,10 @@
 package chapter2;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Created by yuta on 2017/07/05.
  */
@@ -9,9 +14,41 @@ package chapter2;
  */
 public class question15 {
     public static void main(String[] args) {
-        String filepath1 = "res/col1.txt";
-        String filepath2 = "res/col2.txt";
-        String outfilepath = "res/marge_12.txt";
+        String filepath = "res/hightemp.txt";
+        int argument = Integer.valueOf(args[0]);
+        tailprint(filepath,argument);
+
+    }
+
+    public static void tailprint(String filepath,int argument){
+
+        try{
+            FileReader fr = new FileReader(filepath);
+            BufferedReader br = new BufferedReader(fr);
+            //File outfile = new File(outfilepath);
+            //outfile.createNewFile();
+            //PrintWriter pw = new PrintWriter(outfile);
+            String line;
+            String newline;
+            line = br.readLine();
+
+            while(line != null){
+                newline = line.substring(line.length() - argument,line.length());
+                System.out.println(newline);
+                //pw.println(newline);
+                line = br.readLine();
+            }
+
+            br.close();
+            //pw.close();
+
+        }catch (FileNotFoundException e){
+            System.out.println(e);
+
+        }catch (IOException e){
+            System.out.println(e);
+        }
+
 
     }
 
