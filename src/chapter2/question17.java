a9e4d7dc40d863ec740d6a7cfb3cb9acf46261d0
fukuharayuta <fukuhara-yuta-pz@ynu.jp>
2017-07-06 01:06:01 +0900

    question18

--- src/chapter2/question17.java
+++ src/chapter2/question17.java
@@ -1,5 +1,13 @@
 package chapter2;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created by yuta on 2017/07/05.
  */
@@ -9,9 +17,48 @@ package chapter2;
  */
 public class question17 {
     public static void main(String[] args) {
-        String filepath1 = "res/col1.txt";
-        String filepath2 = "res/col2.txt";
-        String outfilepath = "res/marge_12.txt";
+        String filepath = "res/hightemp.txt";
+        List<String> list;
+        list = textset(filepath);
 
+        for(int i =0; i < list.size(); i++){
+            System.out.println(list.get(i));
+        }
+
+    }
+    public static List<String> textset(String filepath){
+        List<String> list = new ArrayList<>();
+        StringBuffer sb = new StringBuffer();
+
+        try{
+            FileReader fr = new FileReader(filepath);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+
+            line = br.readLine();
+
+            while(line != null){
+                String[] linelist = line.split("\t");
+
+                if(list.contains(linelist[0])){
+
+                }else {
+                    list.add(linelist[0]);
+                }
+
+                line = br.readLine();
+            }
+
+            br.close();
+            //pw.close();
+
+        }catch (FileNotFoundException e){
+            System.out.println(e);
+
+        }catch (IOException e){
+            System.out.println(e);
+        }
+
+        return list;
     }
 }
--- src/chapter2/question18.java
+++ src/chapter2/question18.java
@@ -1,5 +1,13 @@
 package chapter2;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by yuta on 2017/07/05.
  */
@@ -9,9 +17,72 @@ package chapter2;
  */
 public class question18 {
     public static void main(String[] args) {
-        String filepath1 = "res/col1.txt";
-        String filepath2 = "res/col2.txt";
-        String outfilepath = "res/marge_12.txt";
+        String filepath = "res/hightemp.txt";
+        reversesort(filepath);
 
     }
+
+    public static void reversesort(String filepath){
+
+        List<Parameters> list = new ArrayList<>();
+
+        try{
+            FileReader fr = new FileReader(filepath);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            //String newline;
+
+            line = br.readLine();
+
+            while(line != null){
+                String[] linelist = line.split("\t");
+                double temperature = Double.parseDouble(linelist[2]);
+                list.add(new Parameters(temperature,line));
+                line = br.readLine();
+            }
+
+            Collections.sort(list);
+            br.close();
+            //pw.close();
+
+            for(Parameters parameters: list){
+                System.out.println(parameters.line);
+            }
+
+        }catch (FileNotFoundException e){
+            System.out.println(e);
+
+        }catch (IOException e){
+            System.out.println(e);
+        }
+    }
+}
+
+class Parameters implements Comparable<Parameters>{
+    String line;
+    double temperature;
+
+    public Parameters(double temperature,String line){
+        this.line=line;
+        this.temperature=temperature;
+    }
+    public String line(){
+        return line;
+    }
+    public double temperature(){
+        return temperature;
+    }
+    @Override
+    public int compareTo(Parameters p){
+        if(this.temperature() >= p.temperature()){
+            return 1;
+        }else if(this.temperature() < p.temperature()){
+            return -1;
+        }else {
+            return 0;
+        }
+
+    }
+
+
 }
