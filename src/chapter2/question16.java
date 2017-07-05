33b926544c59d3e914de8bc762884e263fe0ce92
fukuharayuta <fukuhara-yuta-pz@ynu.jp>
2017-07-05 20:47:35 +0900

    question16終了

--- src/chapter2/question16.java
+++ src/chapter2/question16.java
@@ -1,5 +1,10 @@
 package chapter2;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Created by yuta on 2017/07/05.
  */
@@ -9,9 +14,45 @@ package chapter2;
  */
 public class question16 {
     public static void main(String[] args) {
-        String filepath1 = "res/col1.txt";
-        String filepath2 = "res/col2.txt";
-        String outfilepath = "res/marge_12.txt";
+        String filepath = "res/hightemp.txt";
+        int argument = Integer.valueOf(args[0]);
+
+        argumentsplit(filepath,argument);
+
+    }
+    public static void argumentsplit(String filepath,int argument){
+        try{
+            FileReader fr = new FileReader(filepath);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            String[] newlinelist ={""};
+            int textlength;
+            int splitnumber;
+            int counter=0;
+            line = br.readLine();
+
+            while(line != null){
+                textlength = line.length();
+                splitnumber = textlength / argument;
+
+                //System.out.println(splitnumber);
+
+                for(int i=0; i < textlength; i = i + splitnumber){
+                    System.out.println(line.substring(i,Math.min(textlength,i + splitnumber)));
+                }
+
+                line = br.readLine();
+            }
+
+            br.close();
+            //pw.close();
+
+        }catch (FileNotFoundException e){
+            System.out.println(e);
+
+        }catch (IOException e){
+            System.out.println(e);
+        }
 
     }
 }
