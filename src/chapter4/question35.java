f25624c135aec4fc4267dfd4b2a3bcf6baf41aa5
fukuharayuta <fukuhara-yuta-pz@ynu.jp>
2017-07-10 22:51:19 +0900

    question35終了

--- src/chapter4/question35.java
+++ src/chapter4/question35.java
@@ -1,5 +1,7 @@
 package chapter4;
 
+import java.util.ArrayList;
+
 /**
  * Created by yuta on 2017/07/10.
  */
@@ -10,6 +12,45 @@ package chapter4;
 public class question35 {
     public static void main(String[] args) {
         String filepath = "res/neko2.txt.mecab";
+        nounlongenst(filepath);
 
+    }
+
+    public static void nounlongenst(String filepath) {
+        ArrayList<Dictionary> arrayList;
+        question30 q30 = new question30();
+        arrayList = q30.mecabreader(filepath);
+        StringBuilder sb = new StringBuilder();
+        String pos;
+        String surface;
+        int flag=0;
+
+        for (int i = 0; i < arrayList.size(); i++) {
+            pos = arrayList.get(i).pos;
+            surface = arrayList.get(i).surface;
+
+            if (flag == 0 && pos.equals("名詞")) {
+                //System.out.println("通りました");
+                flag=1;
+                sb.append(surface);
+            }
+            else if (flag >= 1 && pos.equals("名詞")){
+                //System.out.println("通りました");
+                flag=2;
+                sb.append(surface);
+            }
+            else if(flag == 2 && ! pos.equals("名詞")){
+                //System.out.println(pos);
+                //System.out.println(flag);
+                System.out.println("名詞最長連続:" + sb.toString());
+                flag=0;
+                sb.delete(0,sb.length());
+            }
+            else {
+                flag=0;
+                sb.delete(0,sb.length());
+            }
+
+        }
     }
 }
