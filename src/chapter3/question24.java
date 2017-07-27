c1edd488bb0212f94519354dc31f7d67ebf2d8b5
fukuharayuta <fukuhara-yuta-pz@ynu.jp>
2017-07-27 22:47:35 +0900

    question24終了

--- src/chapter3/question24.java
+++ src/chapter3/question24.java
@@ -1,5 +1,13 @@
 package chapter3;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by yuta on 2017/07/07.
  */
@@ -9,6 +17,28 @@ package chapter3;
  */
 public class question24 {
     public static void main(String[] args) {
-        String filepath = "res/hightemp.txt";
+        String filepath = "res/jawiki-uk.json";
+        String regex ="(?:File|ファイル):(.*?)\\|";
+        mediafilesearch(filepath,regex);
+    }
+
+    public static void mediafilesearch(String filepath,String regex){
+        ObjectMapper mapper = new ObjectMapper();
+        Pattern p = Pattern.compile(regex);
+        Matcher matcher;
+
+        try {
+            JsonNode root = mapper.readTree(new File(filepath));
+            matcher = p.matcher(root.get("text").asText());
+
+            while (matcher.find()) {
+                System.out.println(matcher.group(1));
+            }
+
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
     }
 }
