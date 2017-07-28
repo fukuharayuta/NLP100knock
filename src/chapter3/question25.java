cd15ae0a41402ecc184a6cc412bcd87cc3501f77
fukuharayuta <fukuhara-yuta-pz@ynu.jp>
2017-07-28 22:42:41 +0900

    question25半終了

--- src/chapter3/question25.java
+++ src/chapter3/question25.java
@@ -1,5 +1,15 @@
 package chapter3;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by yuta on 2017/07/07.
  */
@@ -9,6 +19,45 @@ package chapter3;
  */
 public class question25 {
     public static void main(String[] args) {
-        String filepath = "res/hightemp.txt";
+        Map<String,String> dictionary = new HashMap<>();
+        String filepath = "res/jawiki-uk.json";
+        String regex ="\\{\\{基礎情報.*([\\s\\S]*?)\\}\\}\\n";
+        String regexdata="\\|(.*?)\\s*(=)\\s*(.*?)(?:(?=\\n\\|)|(?=\\n))";
+        dictionary = templatesearch(filepath,regex,regexdata);
+
+        for(Map.Entry<String,String> entry: dictionary.entrySet()){
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+        }
+
+    }
+
+    public static Map<String,String> templatesearch(String filepath, String regex, String regexdata){
+        Map<String,String> dictionary = new HashMap<>();
+        ObjectMapper mapper = new ObjectMapper();
+        Pattern p = Pattern.compile(regex);
+        Pattern p2 = Pattern.compile(regexdata);
+        Matcher matcher;
+
+        try {
+            JsonNode root = mapper.readTree(new File(filepath));
+            matcher = p.matcher(root.get("text").asText());
+
+            while (matcher.find()) {
+                //System.out.println(matcher.group(1));
+                matcher = p2.matcher(matcher.group(1));
+
+                while(matcher.find()){
+                    dictionary.put(matcher.group(1),matcher.group(3));
+                    //System.out.println(matcher.group(1) + " = "  + matcher.group(3));
+                }
+            }
+
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return dictionary;
+
     }
 }
